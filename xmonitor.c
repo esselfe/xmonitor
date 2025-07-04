39602c3f89cd9736e1c3fabe9f0cfd72d45b30ef
Stephane Fontaine <esselfe16@gmail.com>
2025-07-04 09:45:55 -0400

    push custom setup

--- xmonitor.c
+++ xmonitor.c
@@ -16,32 +16,40 @@ GC gc, gc_buffers;
 time_t t0, tp;
 // May require changes on other systems:
 char *temp_file = "/sys/class/hwmon/hwmon2/temp3_input";
+char *km_file = "/abin/km.log"; // Here, contains us or ca, set by a custom script
 FILE *temp_fp;
+FILE *km_fp;
 XTextItem text_temp;
+XTextItem text_km;
 int text_temp_len;
-unsigned int text_temp_pos_x = 4, text_temp_pos_y = 12;
-unsigned int mem_pos_x = 10, mem_pos_y = 20,
+int text_km_len;
+// local screen dimensions: 1366x768
+unsigned int text_temp_pos_x = 1278, text_temp_pos_y = 696;
+unsigned int text_km_pos_x = 1346, text_km_pos_y = 696;
+unsigned int mem_pos_x = 1277, mem_pos_y = 700,
 	mem_width = 40, mem_height = 40,
-	swap_pos_x = 60, swap_pos_y = 20,
+	swap_pos_x = 1322, swap_pos_y = 700,
 	swap_width = 40, swap_height = 40;
 
 struct history mem_history, swap_history;
 
 void XmonitorSignal(int signum) {
 	Display *dsp = XOpenDisplay(NULL);
-	XClearArea(dsp, window, mem_pos_x, mem_pos_y, mem_width+1, mem_height+1, False);
-	XClearArea(dsp, window, swap_pos_x, swap_pos_y, swap_width+1, swap_height+1, False);
-	XClearArea(dsp, window, text_temp_pos_x, text_temp_pos_y-10, 100, 10, False);
+	XClearArea(dsp, window, mem_pos_x, mem_pos_y, mem_width+4, mem_height+1, False);
+	XClearArea(dsp, window, swap_pos_x, swap_pos_y, swap_width+4, swap_height+1, False);
+	XClearArea(dsp, window, text_temp_pos_x, text_temp_pos_y-10, 120, 10, False);
 	XFlush(dsp);
 	exit(0);
 }
 
 void Draw(void) {
-	XClearArea(display, window, mem_pos_x, mem_pos_y, mem_width, mem_height, False);
-	XClearArea(display, window, swap_pos_x, swap_pos_y, swap_width, swap_height, False);
+	XClearArea(display, window, mem_pos_x, mem_pos_y, mem_width+4, mem_height, False);
+	XClearArea(display, window, swap_pos_x, swap_pos_y, swap_width+4, swap_height, False);
 
-	XClearArea(display, window, text_temp_pos_x, text_temp_pos_y-10, 100, 10, False);
+	XClearArea(display, window, text_temp_pos_x, text_temp_pos_y-10, 120, 10, False);
 	XDrawText(display, window, gc, text_temp_pos_x, text_temp_pos_y, &text_temp, 1);
+	if (km_fp != NULL)
+		XDrawText(display, window, gc, text_km_pos_x, text_km_pos_y, &text_km, 1);
 
 	// Draw contour lines
 	// top
@@ -109,7 +117,7 @@ void UpdateTemp(void) {
 
 	char *line = malloc(64);
 	memset(line, 0, 64);
-	size_t line_size = 64;
+	size_t line_size = 63;
 	ssize_t nread = getline(&line, &line_size, temp_fp);
 	unsigned int val = 0;
 	if (nread > 0) {
@@ -120,6 +128,26 @@ void UpdateTemp(void) {
 
 	fseek(temp_fp, 0, SEEK_SET);
 	fflush(temp_fp);
+
+	free(line);
+}
+
+void UpdateKM(void) {
+	if (km_fp == NULL)
+		return;
+	
+	char *line = malloc(64);
+	memset(line, 0, 64);
+	size_t line_size = 63;
+	ssize_t nread = getline(&line, &line_size, km_fp);
+	if (nread > 0) {
+		line[strlen(line)-1] = '\0';
+		sprintf(text_km.chars, "%s", line);
+		text_km.nchars = strlen(text_km.chars);
+	}
+
+	fseek(km_fp, 0, SEEK_SET);
+	fflush(km_fp);
 
 	free(line);
 }
@@ -137,6 +165,10 @@ int main(int argc, char **argv) {
 	if (temp_fp == NULL)
 		printf("xmonitor error: Cannot open %s: %s\n", temp_file, strerror(errno));
 
+	km_fp = fopen(km_file, "r");
+	if (km_fp == NULL)
+		printf("xmonitor error: Cannot open %s: %s\n", km_file, strerror(errno));
+
 	display = XOpenDisplay(NULL);
 	if (display == NULL) { printf("xmonitor error: Cannot open X display!\n");
 		return 1;
@@ -145,7 +177,7 @@ int main(int argc, char **argv) {
 	window = XDefaultRootWindow(display);
 
 	XGCValues gcv;
-	gcv.foreground = 0xababab;
+	gcv.foreground = 0xa0a8af;
 	gcv.background = 0x101010;
 	gcv.line_width = 1;
 	gcv.line_style = LineSolid;
@@ -175,6 +207,12 @@ int main(int argc, char **argv) {
 	text_temp.nchars = strlen(text_temp.chars);
 	text_temp.delta = 0;
 
+	text_km_len = 4;
+	text_km.chars = malloc(text_km_len);
+	sprintf(text_km.chars, "  ");
+	text_km.nchars = strlen(text_km.chars);
+	text_km.delta = 0;
+
 	HistoryInit(&mem_history, mem_width);
 	HistoryInit(&swap_history, swap_width);
 
@@ -185,6 +223,7 @@ int main(int argc, char **argv) {
 			tp = t0;
 
 			UpdateTemp();
+			UpdateKM();
 			
 			sysinfo(&sinfo);
 			unsigned int bytes_per_pixel = sinfo.totalram / mem_height;
